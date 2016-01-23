532ce3d0b6c2d3dcc08aaa9206fcf551e6e18a6e
william <william@dxt.cn>
2016-01-24 01:08:39 +0800

    修改样式

--- build/freamworks/jPlayer/demo.js
+++ build/freamworks/jPlayer/demo.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
 		{
 			title: "天下",
 			artist: "ADG3",
-			mp3: "http://flatfull.com/themes/assets/musics/adg3com_bustedchump.mp3",
+			mp3: "http://m2.music.126.net/4fEzGRFEKLplsm6QarBHxQ==/1228154488233466.mp3",
 			poster: "images/m0.jpg"
     },
 		{
