bc3ca7faf8260a59e429c4cc7949c87a453555df
biyao <biyao>
2016-03-11 19:04:20 +0800

    重写播放器

--- build/lib/jPlayer/demo.js
+++ build/lib/jPlayer/demo.js
@@ -86,31 +86,4 @@ $(document).ready(function () {
         }
 
     });
-
-
-
-    //	// video
-    //
-    //	$("#jplayer_1").jPlayer({
-    //		ready: function () {
-    //			$(this).jPlayer("setMedia", {
-    //				title: "Big Buck Bunny",
-    //				m4v: "http://flatfull.com/themes/assets/video/big_buck_bunny_trailer.m4v",
-    //				ogv: "http://flatfull.com/themes/assets/video/big_buck_bunny_trailer.ogv",
-    //				webmv: "http://flatfull.com/themes/assets/video/big_buck_bunny_trailer.webm",
-    //				poster: "images/m41.jpg"
-    //			});
-    //		},
-    //		swfPath: "js",
-    //		supplied: "webmv, ogv, m4v",
-    //		size: {
-    //			width: "100%",
-    //			height: "auto",
-    //			cssClass: "jp-video-360p"
-    //		},
-    //		globalVolume: true,
-    //		smoothPlayBar: true,
-    //		keyEnabled: true
-    //	});
-
 });
